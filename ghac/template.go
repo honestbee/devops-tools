f58a7b1a7ec50b8e980b3426cddfe69af7953c26
so0k <vincent.drl@gmail.com>
2017-12-18 00:04:14 +0800

    Initial commit of ghac

---
+++ ghac/template.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"path"
+)
+
+// RenderTemplate renders Team using tplFile into target Writer
+func RenderTemplate(t Team, tplFile string, wr io.Writer) error {
+	tpl, err := template.New(path.Base(tplFile)).
+		ParseFiles(tplFile)
+	if err != nil {
+		return err
+	}
+	return tpl.Execute(wr, t)
+}
---
+++ ghac/templates/team.tf.tpl
@@ -0,0 +1,12 @@
+resource "github_team" "{{.Slug}}" {
+  name        = "{{.Name}}"
+  description = "{{.Description}}"
+  privacy     = "{{.Privacy}}"
+}
+{{ range $user, $role := .UserRoles }}
+resource "github_team_membership" "{{ $.Slug }}-{{ $user }}" {
+  team_id  = "{{ $.ID }}"
+  username = "{{ $user }}"
+  role = "{{ $role }}"
+}
+{{ end }}
