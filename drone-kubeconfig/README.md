d6083b76521f548aca231153c0e6a13aa9a437ad
so0k <vincent.drl@gmail.com>
2018-01-24 12:10:55 +0800

    Add bash script version of kube-kfg

---
+++ drone-kubeconfig/README.md
@@ -0,0 +1,15 @@
+# Drone KubeConfig
+
+Set up Kube API and Token for a drone repo of choice
+
+```
+./drone-kfg.sh -h
+USAGE:
+  drone-kfg <PREFIX> <CONTEXT> <REPO_NAME> : Add Drone secrets for Kubernetes for repo_name
+  drone-kfg -h,--help                      : show this message
+```
+
+This will create the following secrets for `<repo_name>`:
+
+- `<PREFIX>_API_SERVER`: The Kubernetes API server
+- `<PREFIX>_KUBERNETES_TOKEN`: The token for the drone Service Account
---
+++ drone-kubeconfig/drone-kfg.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set -eou pipefail
+
+usage() {
+  cat <<"EOF"
+USAGE:
+  drone-kfg <PREFIX> <CONTEXT> <REPO_NAME> : Add Drone secrets for Kubernetes for repo_name
+  drone-kfg -h,--help                      : show this message
+EOF
+  exit 1
+}
+
+SA="drone"
+NS="kube-system"
+
+set_secrets() {
+  local prefix="${1}"
+  local context="${2}"
+  local repo_name="${3}"
+
+  local cluster=$(kubectl config view -o=jsonpath="{.contexts[?(@.name==\"${context}\")].context.cluster}")
+  local api_server=$(kubectl config view -o=jsonpath="{.clusters[?(@.name==\"${cluster}\")].cluster.server}")
+
+  local secret=$(kubectl --context $context get -n $NS sa $SA -o jsonpath="{.secrets[].name}")
+  local kube_token=$(kubectl --context $context get -n $NS secret $secret -o jsonpath="{.data.token}" | base64 -D)
+  # currently no support for tls verification in drone helm!
+  # local kube_ca=$(kubectl --context $context get -n $NS secret $secret -o jsonpath="{.data.ca\.crt}")
+
+  echo drone secret add --repository ${repo_name} --name ${prefix}_API_SERVER --value ${api_server}
+  echo drone secret add --repository ${repo_name} --name ${prefix}_KUBERNETES_TOKEN --value ${kube_token}
+}
+
+main() {
+  if [[ "${1}" == '-h' || "${1}" == '--help' ]]; then
+    usage
+  elif [[ "$#" -ne 3 ]]; then
+    echo "exactly 3 arguments required"
+    usage
+  else 
+    set_secrets "${1}" "${2}" "${3}"
+  fi
+}
+
+main "$@"
+
