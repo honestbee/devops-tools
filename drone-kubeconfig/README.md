89563ad7dd9d6ab18b5af26749fd59d0b04df728
so0k <vincent.drl@gmail.com>
2018-01-26 03:19:06 +0800

    Add drone-kfg golang tool

--- drone-kubeconfig/README.md
+++ drone-kubeconfig/README.md
@@ -1,6 +1,58 @@
 # Drone KubeConfig
 
-Set up Kube API and Token for a drone repo of choice
+> Retrieve and create Drone secrets for Kubernetes service accounts
+
+Quickly configure Drone to deploy across multiple Kubernetes clusters. 
+
+This script prepares repositories for Kubernetes deployments using the [drone-helm plugin](github.com/ipedrazas/drone-helm/) - which expects  `API_SERVER` and `KUBERNETES_TOKEN` as secrets.
+
+## Pre-reqs
+
+To keep the tool simple, it expects `kubectl` and `drone` binaries to exist in the `$PATH` and be fully configured.
+
+Tested with Kubernetes 1.7.12 and Drone 0.8.0
+
+## Golang binary
+
+```
+NAME:
+   drone-kfg - retrieve and create drone secrets for kubernetes service accounts
+
+USAGE:
+   drone-kfg [global options] command [command options] [arguments...]
+
+VERSION:
+   0.1.0
+
+COMMANDS:
+     help, h  Shows a list of commands or help for one command
+
+GLOBAL OPTIONS:
+   --repository value, -r value                           repository name (e.g. octocat/hello-world)
+   --context PREFIX=KUBE_CONTEXT, -c PREFIX=KUBE_CONTEXT  PREFIX=KUBE_CONTEXT pairs for retrieving drone secrets
+   --service-account value, -s value                      Kubernetes service account for drone (default: "drone")
+   --namespace value, -n value                            Kubernetes namespace of service account (default: "kube-system")
+   --timeout DURATION, -t DURATION                        DURATION before commands are cancelled (default: 5s)
+   --help, -h                                             show help
+   --version, -v                                          print the version
+```
+
+Example usage:
+
+```bash
+bin/drone-kfg -c PRODUCTION_1A=1a.prod.mycluster.com -c PRODUCTION_1B=1b.prod.mycluster.com octocat/hello-world
+```
+
+This will create the following secrets for `octocat/hello-world`:
+
+- `PRODUCTION_1A_API_SERVER`: The API server for the 1A kubernetes production cluster
+- `PRODUCTION_1A_KUBERNETES_TOKEN`: The token for the drone Service Account in the 1A kubernetes production cluster
+- `PRODUCTION_1B_API_SERVER`: The API server for the 1B kubernetes production cluster
+- `PRODUCTION_1B_KUBERNETES_TOKEN`: The token for the drone Service Account in the 1B kubernetes production cluster
+
+## Bash script (deprecated)
+
+Early iteration of drone configuration script - Set up Kube API and Token for a drone repo of choice
 
 ```
 ./drone-kfg.sh -h
---
+++ drone-kubeconfig/main.go
@@ -0,0 +1,249 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"os"
+	"os/exec"
+	"regexp"
+	"time"
+
+	"github.com/urfave/cli"
+	"golang.org/x/net/context"
+)
+
+var build = "0" // build number set at compile-time
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "drone-kfg"
+	app.Version = fmt.Sprintf("0.1.%s", build)
+	app.Usage = "retrieve and create drone secrets for kubernetes service accounts"
+	app.EnableBashCompletion = true
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "repository, r",
+			Usage: "repository name (e.g. octocat/hello-world)",
+		},
+		cli.StringSliceFlag{
+			Name:  "context, c",
+			Usage: "`PREFIX=KUBE_CONTEXT` pairs for retrieving drone secrets",
+		},
+		cli.StringFlag{
+			Name:  "service-account, s",
+			Usage: "Kubernetes service account for drone",
+			Value: "drone",
+		},
+		cli.StringFlag{
+			Name:  "namespace, n",
+			Usage: "Kubernetes namespace of service account",
+			Value: "kube-system",
+		},
+		cli.DurationFlag{
+			Name:  "timeout, t",
+			Usage: "`DURATION` before commands are cancelled",
+			Value: time.Second * 5,
+		},
+		// cli.StringSliceFlag{
+		// 	Name:  "event",
+		// 	Usage: "secret limited to these events",
+		// },
+		// cli.StringSliceFlag{
+		// 	Name:  "image",
+		// 	Usage: "secret limited to these images",
+		// },
+	}
+
+	app.Action = run
+
+	if err := app.Run(os.Args); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
+
+func run(c *cli.Context) error {
+	err := checkBinaries()
+	if err != nil {
+		return err
+	}
+
+	serviceAccount := c.String("service-account")
+	namespace := c.String("namespace")
+	killIn := c.Duration("timeout")
+
+	repoName := c.String("repository")
+	if repoName == "" {
+		repoName = c.Args().First()
+	}
+	if repoName == "" {
+		fmt.Println("missing repository name")
+		cli.ShowAppHelpAndExit(c, 1)
+	}
+
+	ctxMap, err := parseContexts(c.StringSlice("context"))
+	if err != nil {
+		return err
+	}
+
+	if len(ctxMap) < 1 {
+		fmt.Println("missing PREFIX=KUBE_CONTEXT pairs")
+		cli.ShowAppHelpAndExit(c, 1)
+	}
+
+	ctx := context.Background()
+	for prefix, kubeCtx := range ctxMap {
+		fmt.Printf("Setting up %s (with prefix %s) ...\n", kubeCtx, prefix)
+		cluster, err := readKubeConfig(ctx, killIn, fmt.Sprintf(`{.contexts[?(@.name=="%v")].context.cluster}`, kubeCtx))
+		if err != nil {
+			return err
+		}
+		apiServer, err := readKubeConfig(ctx, killIn, fmt.Sprintf(`{.clusters[?(@.name=="%v")].cluster.server}`, cluster))
+		if err != nil {
+			return err
+		}
+		// fmt.Println(apiServer)
+
+
+		fmt.Printf("Retrieving token for service account: '%s' - namespace '%s' (with %v timeout) ...\n", serviceAccount, namespace, killIn)
+		command := []string{
+			"get",
+			"sa",
+			serviceAccount,
+		}
+
+		secretName, err := runKubeCommand(ctx, killIn, kubeCtx, namespace, command, `{.secrets[].name}`)
+		if err != nil {
+			return err
+		}
+
+		command = []string{
+			"get",
+			"secret",
+			secretName,
+		}
+
+		tokenb64, err := runKubeCommand(ctx, killIn, kubeCtx, namespace, command, "{.data.token}")
+		if err != nil {
+			return err
+		}
+
+		token, err := base64.StdEncoding.DecodeString(tokenb64)
+		if err != nil {
+			return err
+		}
+
+		// fmt.Println(string(token))
+		fmt.Printf("Adding secrets for repoName '%s' (with %v timeout) ...\n", repoName, killIn)
+		
+		_, err = addDroneSecret(ctx, killIn, repoName, prefix, "API_SERVER", apiServer)
+		if err != nil {
+			return err
+		}
+		_, err = addDroneSecret(ctx, killIn, repoName, prefix, "KUBERNETES_TOKEN", string(token))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+var contextsExp = regexp.MustCompile(`^([\w-]+)=(.*)`)
+
+// parse prefix=context map string array into a golang map
+func parseContexts(contexts []string) (map[string]string, error) {
+	result := make(map[string]string)
+	for _, kvp := range contexts {
+		matches := contextsExp.FindStringSubmatch(kvp)
+		if len(matches) != 3 {
+			return nil, fmt.Errorf("Invalid context mapping: %s", kvp)
+		}
+		// matches[0] = entire matched string, 1 is first capture group, ...
+		result[matches[1]] = matches[2]
+	}
+	return result, nil
+}
+
+func readKubeConfig(ctx context.Context, killIn time.Duration,  jsonpath string) (string, error) {
+	readConfig := []string{
+		"config",
+		"view",
+		"--output",
+		fmt.Sprintf("jsonpath=\"%s\"", jsonpath),
+	}
+
+	return runCommand(ctx, "kubectl", killIn, readConfig)
+}
+
+func addDroneSecret(ctx context.Context, killIn time.Duration,  repoName string, prefix string, name string, value string) (string, error) {
+	command := []string{
+		"secret",
+		"add",
+		"--repository",
+		repoName,
+		"--name",
+		fmt.Sprintf("%s_%s", prefix, name),
+		"--value",
+		value,
+	}
+	// fmt.Println(command)
+
+	return runDroneCommand(ctx, killIn, command)
+}
+
+func runKubeCommand(ctx context.Context, killIn time.Duration,  kubeCtx string, namespace string, command []string, jsonpath string) (string, error) {
+	command = append(command, "--context", kubeCtx, "--namespace", namespace)
+	command = append(command, "--output", fmt.Sprintf("jsonpath='%s'", jsonpath))
+
+	return runCommand(ctx, "kubectl", killIn, command)
+}
+
+func runDroneCommand(ctx context.Context, killIn time.Duration, command []string) (string, error) {
+	return runCommand(ctx, "drone", killIn, command)
+}
+
+// Run binary in background with timeout, return unquoted output string
+func runCommand(ctx context.Context, binary string, killIn time.Duration, params []string) (string, error) {
+	ctx, _ = context.WithTimeout(ctx, killIn)
+	cmd := exec.CommandContext(ctx, binary, params...)
+	// fmt.Println(params)
+	cmd.Stderr = os.Stderr
+
+	out, err := cmd.Output() //runs the command and returns the output
+
+	if ctx.Err() == context.DeadlineExceeded {
+		fmt.Println("Command timed out")
+		return "", err
+	}
+
+	return trimQuotes(string(out)), err
+}
+
+
+func trimQuotes(s string) string {
+    if len(s) >= 2 {
+        if s[0] == '"' && s[len(s)-1] == '"' {
+            return s[1 : len(s)-1]
+        } else if s[0] == '\'' && s[len(s)-1] == '\'' {
+            return s[1 : len(s)-1]
+        }
+    }
+    return s
+}
+
+func checkBinaries() error {
+	bins := []string{
+		"kubectl",
+		"drone",
+	}
+
+	for _, b := range bins{
+		_ , err := exec.LookPath(b)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
