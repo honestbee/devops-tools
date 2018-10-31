321afdafe8c1d9320d3584d00a0481741290f377
Tuan Nguyen <me@tuannvm.com>
2018-10-31 15:55:47 +0800

    add network testing script

---
+++ network-testing/README.md
@@ -0,0 +1,13 @@
+# Network Testing
+
+Simple script to do network performance testing with [iperf3](https://iperf.fr/)
+
+## Usage
+
+just run `./run.sh`, the script will:
+
+- Deploy iperf3 server & client
+- Proceed on the test
+- Clean up
+
+
---
+++ network-testing/run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eu
+
+kubectl apply -f manifests/
+
+until $(kubectl get pods -l name=iperf3-server -o jsonpath='{.items[0].status.containerStatuses[0].ready}'); do
+    echo "server is not ready yet..."
+    sleep 5
+done
+
+pods=$(kubectl get pods -l name=iperf3-client -o name | cut -d'/' -f2)
+
+for pod in ${pods}; do
+    until $(kubectl get pod ${pod} -o jsonpath='{.status.containerStatuses[0].ready}'); do
+        echo "${pod} is not ready yet..."
+        sleep 5
+    done
+    ip=$(kubectl get pod ${pod} -o jsonpath='{.status.hostIP}')
+    kubectl exec -it ${pod} -- iperf3 -c iperf3-server.default -T "Client on ${ip}" $@
+    echo
+done
+
+kubectl delete -f manifests/
